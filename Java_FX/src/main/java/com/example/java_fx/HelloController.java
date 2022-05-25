b08a879fecd4c2e72022c438b1bf7a8f3f7883c2
MateuszSulenta <100996793+MateuszSulenta@users.noreply.github.com>
2022-05-25 18:52:00 +0200

    Zadanie 4. Połączenie interfejsu z Kontrolerem

--- Java_FX/src/main/java/com/example/java_fx/HelloController.java
+++ Java_FX/src/main/java/com/example/java_fx/HelloController.java
@@ -1,14 +1,29 @@
 package com.example.java_fx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
+
+    int clickCounter = 0;
+    private Button btnMain;
     @FXML
     private Label welcomeText;
+
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+
+
+    public void onBtnAction(ActionEvent actionEvent) {
+        System.out.println("Klikniecie nr " + ++clickCounter);
+        if (clickCounter == 1)
+            btnMain.setText("I jeszcze raz");
+
     }
 }
